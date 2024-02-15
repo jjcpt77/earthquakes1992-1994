7e38b064a15a063d09dc3d7d0f979306c3d86907
EdwardNew <pkarrows@gmail.com>
2024-02-15 14:20:46 -0800

    Got data import working, added d3 install to the project

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from "svelte";
+    import * as d3 from 'd3';
+    let tempData = [];
+
+    onMount(async () => {
+        const res = await fetch('earthquakes_1992_1994.csv'); 
+        const csv = await res.text();
+        tempData = d3.csvParse(csv, d3.autoType)
+        console.log(tempData);
+    });
+</script>
+
+<main>
+    <h1>TESTING!</h1>
+</main>
